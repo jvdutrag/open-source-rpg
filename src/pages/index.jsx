f33fd53c64d396d829bed1b6428df311b4307676
João Dutra <joaovitordutrag@gmail.com>
2021-11-22 20:44:54 -0300

    Added Home Redirect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,20 @@
+import React, { useEffect } from 'react'
+
 import Head from 'next/head'
+import Router from 'next/router'
 
 export default function Home() {
+  useEffect(() => {
+    Router.push('/dashboard');
+  }, []);
+
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
 
-      <h1>Home</h1>
+      <h1>Redirecionando...</h1>
     </div>
   )
 }
